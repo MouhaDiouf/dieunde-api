ae319879a75ba98f4def93caa87d3ea5718fc73a
Mouhamadou <mouhamadoudioufsn@gmail.com>
2021-01-28 11:04:23 +0000

    Update the listener

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
